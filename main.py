d0b502230d6c0a901a7e52f677f0473b9a294dd9
philipAvdey <philipavdey@gmail.com>
2025-02-12 08:49:26 -0800

    added initial searching

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import re
 
-testString = ("int A1=5")
+
+class Token:
+    type = ""
+    token = ""
+
+
+testString = "int A1=5 if "
+
 
 def main():
     cutOneLineTokens(testString)
 
-def cutOneLineTokens(oneLineString):
-    print(oneLineString)
 
-if __name__ == '__main__':
+def cutOneLineTokens(oneLineString):
+    ifMatch = re.search(
+        r"(?<!\S)if(?!\S)",
+        oneLineString,
+    )
+    print(ifMatch)
+
+
+if __name__ == "__main__":
     main()
