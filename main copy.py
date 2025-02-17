2c4a6a703633df15d6bfa2e4beba67540a0cd7a0
philipAvdey <philipavdey@gmail.com>
2025-02-17 10:03:22 -0800

    implemented main functionality

--- main copy.py
+++ main copy.py
@@ -1,40 +1,52 @@
 import re
 
+# list of tuples; regex and the type it is
 regexes = [
-# keys
-r"(?<!\S)if(?!\S)",
-r"(?<!\S)else(?!\S)",
-r"(?<!\S)int(?!\S)",
-r"(?<!\S)float(?!\S)",
-# operators
-r"=",
-r"\+",
-r">",
-r"\*",
-# separators 
-r"\)",
-r"\(",
-r":",
-r"\""
-r";",
-r"(?<!\S)(?!\b(?:int|float|if|else)\b)[a-zA-Z][a-zA-Z0-9]*", # identifier 
-r"(?<!\S)[0-9]+(?!\S)", # int lit
-r"(?<!\S)[0-9]*\.[0-9]+(?!\S)", # double
-r"\"\s*[A-Za-z]*\s*\"", # string lit
+    # keys
+    (r"(?<!\S)if(?!\S)", "key"),
+    (r"(?<!\S)else(?!\S)", "key"),
+    (r"(?<!\S)int(?!\S)", "key"),
+    (r"(?<!\S)float(?!\S)", "key"),
+    # operators
+    (r"\+", "operator"),
+    (r">", "operator"),
+    (r"\*", "operator"),
+    # separators
+    (r"\)", "separator"),
+    (r"\(", "separaotr"),
+    (r":", "separaotr"),
+    (r"\"" r";", "separaotr"),
+    (r"(?<!\S)(?!\b(?:int|float|if|else)\b)[a-zA-Z][a-zA-Z0-9]*", "identifier"),
+    (r"(?<!\S)[0-9]+(?!\S)", "int lit"),
+    (r"(?<!\S)[0-9]*\.[0-9]+(?!\S)", "float"),
+    (r"\"\s*[A-Za-z]*\s*\"", "string lit"),
+    (r"=", "operator"),
 ]
 
 
-testString = "int A1=5;"
+testString = "int A1 = 5;"
+results = []  # list of <token, type> strings
 
 
 def main():
     cutOneLineTokens(testString)
 
 
-
 def cutOneLineTokens(oneLineString):
-    print(oneLineString)
-    
+    for regex in regexes:
+        match = re.match(regex[0], oneLineString)
+        if match:  # if we matched something, add it to the results list
+            # main issues: how do we determine type? also, what if there are multiple matches of the same type?
+            results.append("<" + match.group(0) + ">")
+
+            # we have to cut out the token we just matched
+            firstHalf = oneLineString[0 : match.start()] 
+            secondHalf = oneLineString[match.end() :]
+            oneLineString = firstHalf + secondHalf
+            oneLineString = oneLineString.lstrip() # strip leading whitespaces 
+
+    print(results)
+
 
 if __name__ == "__main__":
     main()
